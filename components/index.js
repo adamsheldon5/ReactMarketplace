c96b66689d21dc9d6a4ffc78a83f9c576879a030
Adam Sheldon <adamsheldon5@gmail.com>
2022-05-24 17:52:06 +0100

    First upload - App skeleton with dummy placeholders

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTcard from "./NFTcard";
+import { CircleButton, RectButton } from "./button";
+
+export  {
+    HomeHeader, NFTcard, FocusedStatusBar
+}
